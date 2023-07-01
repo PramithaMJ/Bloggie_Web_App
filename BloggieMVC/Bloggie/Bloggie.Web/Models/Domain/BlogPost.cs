f8983900a3393a29dda464011f80973b25747497
PramithaMJ <lpramithamj@gmail.com>
2023-07-02 00:49:42 +0530

    Icollection

--- BloggieMVC/Bloggie/Bloggie.Web/Models/Domain/BlogPost.cs
+++ BloggieMVC/Bloggie/Bloggie.Web/Models/Domain/BlogPost.cs
@@ -2,5 +2,17 @@
 {
     public class BlogPost
     {
+        public Guid Id { get; set; } //Guid = uniqueidentifier data type
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }    
+        public DateTime PublishedDate { get; set; }
+        public bool Author { get; set; }
+        public bool Visible { get; set; }
+
+        public ICollection<Tag> Tags { get; set; } //BlogPosts have multiple Tags
     }
 }
