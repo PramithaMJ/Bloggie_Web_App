5ebe29f1a7a763ba3729dd45e450af46fee7e00e
PramithaMJ <lpramithamj@gmail.com>
2023-07-02 01:16:46 +0530

    add comment

--- BloggieMVC/Bloggie/Bloggie.Web/Data/BloggieDbContext.cs
+++ BloggieMVC/Bloggie/Bloggie.Web/Data/BloggieDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace Bloggie.Web.Data
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
 {
-    public class BloggieDbContext
+    public class BloggieDbContext: DbContext
     {
+        //Constroctor
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; } //Create table
+
+        public DbSet<Tag> Tags { get; set; } // Create table
     }
 }
